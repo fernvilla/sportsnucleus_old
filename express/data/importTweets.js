c981fa342e3254b58e0aa693f11a734fadcd0492
fernvilla <fvilla18@gmail.com>
2018-06-03 21:42:57 -0700

    improve calculating when tweets are saved to disconnect db

--- express/data/importTweets.js
+++ express/data/importTweets.js
@@ -13,66 +13,88 @@ const T = new Twit({
   timeout_ms: 60 * 1000
 });
 
-mongoose.connection.on('connected', () => {
-  initParser();
-});
+mongoose.connection.on('connected', () => initParser());
 
-const disconnectDb = () => {
-  mongoose.disconnect();
-};
-
-let processedAccounts = 0;
-let totalAccounts = 0;
+const disconnectDb = () => mongoose.disconnect();
 
 const initParser = () => {
   TwitterAccount.find({}).exec((err, twitterAccounts) => {
     if (err || !twitterAccounts.length) return disconnectDb();
 
     totalAccounts = twitterAccounts.length;
-    twitterAccounts.map(twitterAccount => fetchTweets(twitterAccount));
+    twitterAccounts.map(twitterAccount => fetchTweets(twitterAccount.screenName));
   });
 };
 
-const fetchTweets = twitterAccount => {
+let processedAccounts = 0;
+let totalAccounts = 0;
+
+const fetchTweets = screenName => {
+  let processedTweets = 0;
+  let totalTweets = 0;
+
+  const checkAllTweetsProcessed = () => {
+    processedTweets++;
+
+    if (processedTweets === totalTweets) {
+      checkForDisconnect();
+    }
+  };
+
   T.get(
     'statuses/user_timeline',
     {
-      screen_name: twitterAccount.screenName,
+      screen_name: screenName,
       count: 30,
       include_rts: true,
       exclude_replies: true
     },
     (err, data, response) => {
-      if (err) return console.log(err);
+      if (err) {
+        checkForDisconnect();
+        return console.log('twitter response error: ', err);
+      }
 
-      return data.map(d => {
+      totalTweets = data.length;
+
+      data.map(d => {
         const media = d.entities.media;
-
         const tweet = new Tweet({
           text: d.text,
           tweetId: d.id_str,
           published: d.created_at,
           userName: d.user.name,
           profileImageUrl: d.user.profile_image_url,
-          twitterAccount: twitterAccount._id,
           imageUrl: media ? media[0].media_url_https : null
         });
 
-        tweet.save((err, tweet) => {
+        TwitterAccount.findOne({ screenName }, (err, twitterAccount) => {
           if (err) {
-            checkForDisconnect();
-            return console.log(err);
+            checkAllTweetsProcessed();
+            return console.log(`find twitter account error: ${err}`);
           }
 
-          twitterAccount.tweets.push(tweet);
+          tweet.twitterAccount = twitterAccount._id;
 
-          twitterAccount.save((err, twitterAccount) => {
+          tweet.save((err, tweet) => {
             if (err) {
-              return console.log(err);
+              checkAllTweetsProcessed();
+
+              return console.log(`tweet save error: ${err}`);
             }
 
-            checkForDisconnect();
-            console.log('Tweet created: ', tweet);
+            twitterAccount.tweets.push(tweet);
+
+            twitterAccount.save(err => {
+              if (err) {
+                checkAllTweetsProcessed();
+
+                return console.log(`twitterAccount save error: ${err}`);
+              }
+
+              console.log(`Tweet created: ${tweet}`);
+              checkAllTweetsProcessed();
+            });
           });
         });
       });
