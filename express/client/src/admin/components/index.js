2f6afe940bcfbf7573980958a44e3ce94da9593f
fernvilla <fvilla18@gmail.com>
2018-06-03 23:45:12 -0700

    Remove folder copy

--- express/client/src/admin/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as App } from './App';
-export { default as Dashboard } from './Dashboard';
-export { default as Leagues } from './Leagues';
-export { default as Nav } from './Nav';
