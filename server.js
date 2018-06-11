467ec1e5e4253019aee655f9b15a525f5e409016
Fernando Villasenor <fernvilla@users.noreply.github.com>
2018-06-11 09:02:14 -0700

    Fix server port number

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.get('*', (req, res) => {
 });
 
 // Run server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
